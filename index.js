ae7cf3a90f397ce3b082e413aec99c418d25a7a9
realvineeths <vineeth14shivu@gmail.com>
2023-01-08 11:01:45 +0530

    automated only

--- index.js
+++ index.js
@@ -1,45 +1,57 @@
-
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const app = require('express');
-const http = require('http')
+const http = require('http');
+const ejs= require('ejs');
+const path=require('path');
 
 
-  (async () => {
-
-    var type = process.argv.slice(2)[0] || 'url';
-
+(async () => {
     const browser = await puppeteer.launch();
 
     const page = await browser.newPage();
 
+    try {
+      var compiled = ejs.compile(fs.readFileSync(__dirname + '/views/cert1.ejs', 'utf8'));
+      var cname='VINAY';
+      var html = compiled({ name: cname, title:'Node Developer' , score:'A+',date:'29 december,2022'});//DYNAMIC VALUES
 
-    if (type === 'url') {
-
-      const website_url = 'https://vcint.github.io/nodeapi/';
-
-      await page.goto(website_url, { waitUntil: 'networkidle0' });
-
-    } else if (type === 'file') {
-
-      const html = fs.readFileSync('sample.html', 'utf-8');
       await page.setContent(html, { waitUntil: 'domcontentloaded' });
-
-    } else {
-
-      console.log(new Error(`HTML source "${type}" is unkown.`));
+    } catch (error) {
+      console.log(new Error(`${error}`));
       await browser.close();
       return;
     }
+
     await page.emulateMediaType('screen');
 
     const pdf = await page.pdf({
-      path: `result_${type}.pdf`,
+      path: `${cname}.pdf`,
       margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
       printBackground: true,
-      format: 'A4',
+      format: 'letter',
     });
-
     await browser.close();
-    res.send(pdf);
-  })();
+    
+})();
+
+
+
+// var type = process.argv.slice(2)[0] || 'url';
+
+
+// if (type === 'url') {
+
+//   const website_url = 'https://vcint.github.io/nodeapi/';
+
+//   await page.goto(website_url, { waitUntil: 'networkidle0' });
+
+// } else if (type === 'file') {
+
+
+// } else {
+
+//   console.log(new Error(`HTML source "${type}" is unkown.`));
+//   await browser.close();
+//   return;
+// }
